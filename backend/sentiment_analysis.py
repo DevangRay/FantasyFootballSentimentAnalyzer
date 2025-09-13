3c9d047872bae30eba9f605162cd8887fc99734d
Devang Ray <devangray624@gmail.com>
2025-09-13 15:41:53 -0400

    added documentation. create final_player_object with fuzzy_matched name, status, and match score to be passed into sentiment analysis

--- backend/sentiment_analysis.py
+++ backend/sentiment_analysis.py
@@ -33,14 +33,19 @@ def clean_name_and_sentence_of_nicknames(name: str, sentence: str) -> str:
     return name, sentence
 
 def process_transcript(transcript_filepath: str) -> list[str]:
+    # read transcript file to variable raw_transcript
     raw_transcript = open(transcript_filepath, "r").read()
     
+    # use spacy to split raw_transcript into sentences and identify named entities
     doc = nlp(raw_transcript)
     sentences = list(doc.sents)
+
+    # create list of stringified sentences and write to sentences.json    
     stringified_sentences = [sent.text.strip() for sent in sentences]
     with open("../resources/sentences.json", "w", encoding="utf-8") as f:
         json.dump(stringified_sentences, f, ensure_ascii=False, indent=2)
     
+    # create list of identified names, associated with the sentence index and the sentence itself
     identified_names = []
     for i, sent in enumerate(sentences, start=0):
         names = [ent.text for ent in sent.ents if ent.label_ == "PERSON"]
@@ -53,30 +58,52 @@ def process_transcript(transcript_filepath: str) -> list[str]:
                     "sentence": clean_sentence
                 })
     
-    for identified_name in identified_names:
-        print(f"{identified_name['name']} | {identified_name['sentence_index']} | {identified_name['sentence']}")
+    # for identified_name in identified_names:
+    #     print(f"{identified_name['name']} | {identified_name['sentence_index']} | {identified_name['sentence']}")
     
-    # # nfl.get_nfl_players()
+    # save list of real NFL players to nfl_player_roster
+    # nfl.get_nfl_players()
     nfl_player_roster = []
     with open("../resources/nfl_roster.json", "r") as f:
         nfl_player_roster = json.load(f)
-    # print(len(nfl_player_roster)) # --> 6172
-    
-    for player_object in identified_names:
+
+    # fuzzy match identified names to nfl_player_roster, and save in final_player_object
+    final_player_object = []
+    for index, player_object in enumerate(identified_names, start=0):
         player = player_object['name']
         closest_player_list = process.extract(player, nfl_player_roster, limit=5, scorer=fuzz.token_set_ratio)
-        
-        possible_matches = [close_player[0] for close_player in closest_player_list if close_player[1] == 100]
+                
+        possible_matches = [close_player for close_player in closest_player_list if close_player[1] == 100]
         if len(possible_matches) < 1: 
-            possible_matches = [close_player[0] for close_player in closest_player_list if close_player[1] > 80]
-        
+            possible_matches = [close_player for close_player in closest_player_list if close_player[1] > 80]
+       
         final_name = ""
-        if (len(possible_matches) > 0):
-            final_name = " | ".join(possible_matches) + f" ({player})"
+        status = ""
+        if (len(possible_matches) == 1): 
+            # perfect match
+            final_name = possible_matches[0][0]
+            status = "perfect match"
+        elif (len(possible_matches) > 1):
+            # multiple matches
+            # final_name = " | ".join(possible_matches)
+            final_name = possible_matches[0][0]
+            status = "best of multiple matches"
         else:
-            final_name = f"NO MATCH => {player}"
-            
-        print(f" {final_name} | {player_object['sentence_index']} | {player_object['sentence']}")
+            # no matches
+            final_name = player
+            status = "no match"
+        
+        final_player_object.append({
+            "transcript_name": player,
+            "matched_name": final_name,
+            "score": possible_matches[0][1] if len(possible_matches) > 0 else 0,
+            "status": status,
+            "sentence_index": player_object['sentence_index'],
+            "sentence": player_object['sentence']
+        })
+    
+    sorted_final_player_object = sorted(final_player_object, key=lambda x: x['matched_name'].lower()) 
+    print(sorted_final_player_object)
     
 
 def main():
