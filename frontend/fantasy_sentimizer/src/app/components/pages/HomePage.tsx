2a82ee75483d61f02d7a9562938d9f0921af2b1e
Devang Ray <devangray624@gmail.com>
2026-03-15 21:07:28 -0400

    added demo button

--- frontend/fantasy_sentimizer/src/app/components/pages/HomePage.tsx
+++ frontend/fantasy_sentimizer/src/app/components/pages/HomePage.tsx
@@ -9,13 +9,30 @@ import { Button } from "@/components/ui/button";
 export default function HomePage() {
     const [submittedText, setSubmittedText] = useState<string | null>(null);
 
+    async function onDemoClick () {
+        try {
+            const response = await fetch("transcript.txt");
+            const text = await response.text();
+            setSubmittedText(text);
+        } catch (e) {
+            console.error("Error loading demo transcript: ", e);
+        }
+    }
+
     return (
-        <div className="flex-1 flex flex-col min-h-0">
+        <div className="flex-1 flex flex-col min-h-0 gap-4">
             {
                 !submittedText ?
                     <>
                         {/* Body */}
                         <TextUpload setSubmittedText={setSubmittedText} />
+
+                        <Button 
+                            className="max-w-fit mx-auto cursor-pointer"
+                            onClick={onDemoClick}
+                        >
+                            Try a Demo
+                        </Button>
 
                         {/* Footer */}
                         <FileUploadDialog setSubmittedText={setSubmittedText} />
